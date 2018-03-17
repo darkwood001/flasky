24d2595427ffef6dc35fa566b32ff298fa278487
einino <darkwood001@gmail.com>
2018-03-17 10:57:07 +0800

    5b update

--- hello.py
+++ hello.py
@@ -51,6 +51,7 @@ def index():
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
+            db.session.commit()
             session['known'] = False
         else:
             session['known'] = True
